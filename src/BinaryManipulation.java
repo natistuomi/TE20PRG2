c4846953db6d3d9ce00f9ba9f50a1e740a517dd6
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-09-02 13:09:53 +0200

    Kryptering av sträng?

--- src/BinaryManipulation.java
+++ src/BinaryManipulation.java
@@ -3,16 +3,41 @@ public class BinaryManipulation {
     public static void main(String[] args){
         char m = 'H';
         char k = '(';
-        System.out.println((char)cryptInfo(m,k));
-        System.out.println((char)decryptInfo(k,(char)cryptInfo(m,k)));
+        System.out.println((char)cryptChar(m,k));
+        System.out.println((char)decryptChar(k,(char)cryptChar(m,k)));
+        String message = "TESTmeddelande";
+        String key = "123";
+        System.out.println(cryptString(message, key));
+        System.out.println(decryptString(key, cryptString(message, key)));
     }
 
-    public static int cryptInfo(char m, char k) {
+    public static int cryptChar(char m, char k) {
         return m^k;
     }
 
-    public static int decryptInfo(char k, char c){
+    public static int decryptChar(char k, char c){
         return k^c;
     }
 
+    public static String cryptString(String m, String k){
+        String s = "";
+        int keyLength = k.length();
+        for(int i = 0; i < m.length(); i++){
+            char x = m.charAt(i);
+            char y = k.charAt(i%keyLength);
+            s = s + (char)cryptChar(x,y);
+        }
+        return s;
+    }
+
+    public static String decryptString(String k, String c){
+        String s = "";
+        int keyLength = k.length();
+        for(int i = 0; i < c.length(); i++){
+            char x = c.charAt(i);
+            char y = k.charAt(i%keyLength);
+            s = s + (char)cryptChar(x,y);
+        }
+        return s;
+    }
 }
